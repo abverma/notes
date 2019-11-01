9868629115c89a53c93d885560691a536cae3250
Simon Goback <abverma@protonmail.com>
2019-11-01 14:52:44 +0530

    Initial commit

---
+++ bin/note-global.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const handler = require('../lib/index.js')
+
+if (process.argv[2]) {
+
+	switch(process.argv[2]) {
+		case '-c': mode = 'new'
+			  	   break
+		case '-a': mode = 'append'
+			  	   break
+		case '-l': mode = 'list'
+			  	   break
+		case '-s': mode = 'search'
+			  	   break
+		case '-e': mode = 'edit'
+			  	   break
+		case '-S': mode = 'sync'
+			  	   break
+		case '-Sa': mode = 'syncall'
+			   		break
+	}
+
+	title = process.argv[3]
+	note = process.argv[4]
+
+	handler.handleRequest(mode, title, note)
+}
